5946f44fbd4362bde0f785a5860bdb17cb7faf52
Marek Nevole <nevole.marek@gmail.com>
2021-11-25 18:09:08 +0100

    Add SimulatedAnnealingWeightedSAT solver class and minor optimization to abstract class

--- simulatedannealing.py
+++ simulatedannealing.py
@@ -3,6 +3,8 @@ from random import random
 
 import math
 
+from utils import FormulaAdjacencyList
+
 class SimulatedAnnealing(ABC):
     """ Abstract class of simulated annealing. """
     def __init__(
@@ -56,15 +58,15 @@ class SimulatedAnnealing(ABC):
     def run(self):
         ITER_LIMIT = self._iter_limit
         RESTART_LIMIT = self._restart_limit
+        TEMP = self._initial_temperature()
 
         best_score = 0
         best_state = None
 
         for _ in range(RESTART_LIMIT):
-
             current_state = self._initial_state()
             current_score = self._evaluate(current_state)
-            temperature = self._initial_temperature()
+            temperature = TEMP
 
             iter_count = 0
             while iter_count < ITER_LIMIT:
@@ -76,14 +78,55 @@ class SimulatedAnnealing(ABC):
                     current_state = next_state
                     current_score = next_score
                     iter_count = 0
+
+                    if current_score > best_score:
+                        best_score = current_score
+                        best_state = current_state
                 else:
                     iter_count += 1
 
                 temperature = self._cooling_schedule(temperature)
-
-                if current_score > best_score:
-                    best_score = current_score
-                    best_state = current_state
-
         return best_score, best_state
 
+
+class SA_WeightedSAT(SimulatedAnnealing):
+    def __init__(
+        self,
+        formula,
+        iter_limit,
+        restart_limit,
+        alpha = 0.99,
+        beta = 0,
+        temp_prob = 0.8,
+        init_state = '',
+        next_state = ''
+    ):
+
+        super().__init__(
+            iter_limit,
+            restart_limit
+        )
+        self.formula = formula
+        self.alpha = alpha
+        self.beta = beta
+        self.temp_prob = temp_prob
+        self.eval = eval
+        self.init_state = init_state
+        self.next_state = next_state
+        self.adj_list = FormulaAdjacencyList(formula)
+        self.clause_weight = sum(formula.weights) + 1
+
+    def _initial_state(self):
+        pass
+
+    def _evaluate(self, state):
+        pass
+
+    def _next_state(self, current_state):
+        pass
+
+    def _cooling_schedule(self, temperature):
+        pass
+
+    def _initial_temperature(self):
+        pass
